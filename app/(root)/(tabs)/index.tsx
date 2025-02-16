71708575e2e0dcbb3f423d95868eb2278827fa59
DavidPlitnizki <dplitnizki@gmail.com>
2025-02-16 14:45:27 +0200

    inprogress

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,18 +1,69 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import banners from "@/constants/banners";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { useEffect, useRef, useState } from "react";
+import { Text, View, Image, ImageSourcePropType } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+type BannerType = {
+  [key: number]: ImageSourcePropType | undefined;
+};
+
+const MapBanners: BannerType = {
+  1: banners.banner_1,
+  2: banners.banner_2,
+  3: banners.banner_3,
+};
 
 export default function Index() {
+  const [banner, setBanner] = useState(1);
+  const intervalRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  useEffect(() => {
+    const changeBanner = () => {
+      if (banner > 2) {
+        setBanner(1);
+      } else {
+        setBanner((prev) => (prev += 1));
+      }
+    };
+    intervalRef.current = setInterval(() => {
+      changeBanner();
+    }, 3000);
+
+    return () => {
+      if (intervalRef.current) {
+        clearInterval(intervalRef.current);
+      }
+    };
+  }, [banner]);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="font-bold text-3xl font-rubik my-10">
-        Welcome to Restate
-      </Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="px-4">
+        <View className="flex flex-row justify-between items-center">
+          <View className="flex flex-row items-end">
+            <Image
+              source={images.avatar}
+              resizeMode="contain"
+              className="w-12 h-12"
+            />
+            <Text className="font-rubik-medium ml-2 text-2xl text-black-300">
+              User Name
+            </Text>
+          </View>
+          <View>
+            <Image
+              source={icons.bell}
+              resizeMode="contain"
+              className="w-8 h-8"
+            />
+          </View>
+        </View>
+
+        <View className="flex mt-4">
+          <Image source={MapBanners[banner]} className="w-full" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
---
+++ constants/banners.ts
@@ -0,0 +1,9 @@
+import banner_1 from "../assets/banners/01.png";
+import banner_2 from "../assets/banners/02.png";
+import banner_3 from "../assets/banners/03.png";
+
+export default {
+  banner_1,
+  banner_2,
+  banner_3,
+};
